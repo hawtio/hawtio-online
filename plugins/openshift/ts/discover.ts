6b26ac7ac4584af268b8f1cc9465b06cbd0d1bf0
Antonin Stefanutti <antonin@stefanutti.fr>
2017-02-13 15:21:38 +0100

    Add toolbar and list layout

--- plugins/openshift/ts/discover.ts
+++ plugins/openshift/ts/discover.ts
@@ -9,12 +9,14 @@ module Openshift {
     ['$scope', '$location', '$element', 'K8SClientFactory', 'jsonpath', 'userDetails',
       ($scope, $location, $element, client: K8SClientFactory, jsonpath, userDetails) => {
 
-        $scope.pods = [];
+        $scope.pods         = [];
+        $scope.filteredPods = [];
 
         const kubernetes = client.create('pods');
         const handle     = kubernetes.watch(pods => {
           $scope.pods.length = 0;
           $scope.pods.push(..._.filter(pods, pod => jsonpath.query(pod, '$.spec.containers[*].ports[?(@.name=="jolokia")]').length > 0));
+          applyFilters($scope.filterConfig.appliedFilters);
           // have to kick off a $digest here
           $scope.$apply();
         });
@@ -24,14 +26,62 @@ module Openshift {
         $element.on('$destroy', _ => $scope.$destroy());
         $scope.$on('$destroy', _ => K8SClientFactory.destroy(kubernetes, handle));
 
-        $scope.config = {
+        const applyFilters = filters => {
+          $scope.filteredPods.length = 0;
+          if (filters && filters.length > 0) {
+            $scope.pods.forEach(pod => {
+              if (_.every(filters, filter => matches(pod, filter))) {
+                $scope.filteredPods.push(pod);
+              }
+            });
+          } else {
+            $scope.filteredPods.push(...$scope.pods);
+          }
+          $scope.toolbarConfig.filterConfig.resultsCount = $scope.filteredPods.length;
+        };
+
+        const matches = (item, filter) => {
+          let match = true;
+          if (filter.id === 'name') {
+            match = item.metadata.name.match(filter.value) !== null;
+          } else if (filter.id === 'namespace') {
+            match = item.metadata.namespace.match(filter.value) !== null;
+          }
+          return match;
+        };
+
+        $scope.filterConfig = {
+          fields        : [
+            {
+              id         : 'name',
+              title      : 'Name',
+              placeholder: 'Filter by Name...',
+              filterType : 'text'
+            },
+            {
+              id         : 'namespace',
+              title      : 'Namespace',
+              placeholder: 'Filter by Namespace...',
+              filterType : 'text'
+            }
+          ],
+          resultsCount  : $scope.filteredPods.length,
+          appliedFilters: [],
+          onFilterChange: applyFilters
+        };
+
+        $scope.toolbarConfig = {
+          filterConfig: $scope.filterConfig,
+        };
+
+        $scope.listConfig = {
           selectItems       : false,
           multiSelect       : false,
           dblClick          : false,
           dragEnabled       : false,
           selectionMatchProp: 'name',
           selectedItems     : [],
-          showSelectBox     : true,
+          showSelectBox     : false,
           useExpandingRows  : false
         };
 
@@ -53,7 +103,7 @@ module Openshift {
           window.open(connectUrl.toString());
         };
 
-        $scope.actions = [
+        $scope.podActions = [
           {
             name    : 'Connect',
             class   : 'btn-primary',
