338390cc066112ef21e23974b100233f73227b2a
Antonin Stefanutti <antonin@stefanutti.fr>
2017-02-15 10:52:44 +0100

    Fix dependency on Kubernetes API

--- plugins/includes.ts
+++ plugins/includes.ts
@@ -1,2 +1,2 @@
 /// <reference path="../libs/hawtio-utilities/defs.d.ts"/>
-/// <reference path="../libs/hawtio-kubernetes/defs.d.ts"/>
+/// <reference path="../libs/hawtio-kubernetes-api/defs.d.ts"/>
