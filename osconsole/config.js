823f3de6e2135bdd6af908d8feda8ab597522b19
Antonin Stefanutti <antonin@stefanutti.fr>
2017-02-24 11:20:10 +0100

    Fix Hawtio OAuth plugin config

--- osconsole/config.js
+++ osconsole/config.js
@@ -1,6 +1,5 @@
 window.OPENSHIFT_CONFIG = window.HAWTIO_OAUTH_CONFIG = {
-  master_uri: 'https://open.paas.redhat.com/',
-  auth: {
+  openshift: {
     oauth_authorize_uri: 'https://open.paas.redhat.com/oauth/authorize',
     oauth_client_id: 'system:serviceaccount:hawtio:hawtio-oauth-client',
     scope: 'user:info user:check-access role:edit:hawtio'
