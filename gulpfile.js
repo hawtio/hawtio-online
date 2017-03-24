7ec07470ad86c2446dd969ed4673ffc3565afd86
Antonin Stefanutti <antonin@stefanutti.fr>
2017-03-24 15:46:17 +0100

    Use arrow functions

--- gulpfile.js
+++ gulpfile.js
@@ -37,25 +37,18 @@ const config = {
   })
 };
 
-gulp.task('bower', function () {
-  return gulp.src('index.html')
-    .pipe(wiredep({}))
-    .pipe(gulp.dest('.'));
-});
+gulp.task('bower', () => gulp.src('index.html')
+  .pipe(wiredep({}))
+  .pipe(gulp.dest('.')));
 
 /** Adjust the reference path of any typescript-built plugin this project depends on */
-gulp.task('path-adjust', function () {
-  return gulp.src('libs/**/includes.d.ts')
-    .pipe(plugins.replace(/"\.\.\/libs/gm, '"../../../libs'))
-    .pipe(gulp.dest('libs'));
-});
+gulp.task('path-adjust', () => gulp.src('libs/**/includes.d.ts')
+  .pipe(plugins.replace(/"\.\.\/libs/gm, '"../../../libs'))
+  .pipe(gulp.dest('libs')));
 
-gulp.task('clean-defs', function () {
-  return del('defs.d.ts');
-});
+gulp.task('clean-defs', () => del('defs.d.ts'));
 
 gulp.task('tsc', ['clean-defs'], function () {
-  const cwd      = process.cwd();
   const tsResult = gulp.src(config.ts)
     .pipe(plugins.typescript(config.tsProject))
     .on('error', plugins.notify.onError({
@@ -72,74 +65,61 @@ gulp.task('tsc', ['clean-defs'], function () {
       .pipe(gulp.dest('d.ts')))
     .pipe(plugins.filter('**/*.d.ts'))
     .pipe(plugins.concatFilenames('defs.d.ts', {
-      root   : cwd,
+      root   : process.cwd(),
       prepend: '/// <reference path="',
       append : '"/>'
     }))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('template', ['tsc'], function () {
-  return gulp.src(config.templates)
-    .pipe(plugins.angularTemplatecache({
-      filename      : 'templates.js',
-      root          : 'plugins/',
-      standalone    : true,
-      module        : config.templateModule,
-      templateFooter: '}]); hawtioPluginLoader.addModule("' + config.templateModule + '");'
-    }))
-    .pipe(gulp.dest('.'));
-});
+gulp.task('template', ['tsc'], () => gulp.src(config.templates)
+  .pipe(plugins.angularTemplatecache({
+    filename      : 'templates.js',
+    root          : 'plugins/',
+    standalone    : true,
+    module        : config.templateModule,
+    templateFooter: '}]); hawtioPluginLoader.addModule("' + config.templateModule + '");'
+  }))
+  .pipe(gulp.dest('.')));
 
-gulp.task('concat', ['template'], function () {
-  return gulp.src(['compiled.js', 'templates.js'])
+gulp.task('concat', ['template'], () =>
+  gulp.src(['compiled.js', 'templates.js'])
     .pipe(plugins.concat(config.js))
-    .pipe(gulp.dest(config.dist));
-});
+    .pipe(gulp.dest(config.dist)));
 
-gulp.task('clean', function () {
-  return del(['templates.js', 'compiled.js', './site/']);
-});
+gulp.task('clean', () => del(['templates.js', 'compiled.js', './site/']));
 
-gulp.task('less', function () {
-  return gulp.src(config.less)
-    .pipe(plugins.less({
-      paths: [path.join(__dirname, 'libs')]
-    }))
-    .on('error', plugins.notify.onError({
-      onLast : true,
-      message: '<%= error.message %>',
-      title  : 'less file compilation error'
-    }))
-    .pipe(plugins.concat(config.css))
-    .pipe(gulp.dest(config.dist));
-});
+gulp.task('less', () => gulp.src(config.less)
+  .pipe(plugins.less({
+    paths: [path.join(__dirname, 'libs')]
+  }))
+  .on('error', plugins.notify.onError({
+    onLast : true,
+    message: '<%= error.message %>',
+    title  : 'less file compilation error'
+  }))
+  .pipe(plugins.concat(config.css))
+  .pipe(gulp.dest(config.dist)));
 
 gulp.task('watch-less', function () {
-  plugins.watch(config.less, function () {
-    gulp.start('less');
-  });
+  plugins.watch(config.less, () => gulp.start('less'));
 });
 
 gulp.task('watch', ['build', 'watch-less'], function () {
-  plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', urljoin(config.dist, '*')], function () {
-    gulp.start('reload');
-  });
-  plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], function () {
-    gulp.start(['tsc', 'template', 'concat', 'clean']);
-  });
+  plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', urljoin(config.dist, '*')], () => gulp.start('reload'));
+  plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], () => gulp.start(['tsc', 'template', 'concat', 'clean']));
 });
 
 gulp.task('connect', ['watch'], function () {
   // lets disable unauthorised TLS issues with kube REST API
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
   const kubeBase = process.env.KUBERNETES_MASTER || 'https://open.paas.redhat.com/';
-  console.log("==== using KUBERNETES URL: " + kubeBase);
+  console.log('==== using KUBERNETES URL: ' + kubeBase);
   const kube     = uri(urljoin(kubeBase, 'api'));
   const kubeapis = uri(urljoin(kubeBase, 'apis'));
   const oapi     = uri(urljoin(kubeBase, 'oapi'));
-  console.log("Connecting to Kubernetes on: " + kube);
+  console.log('Connecting to Kubernetes on: ' + kube);
 
   hawtio.setConfig({
     logLevel     : logger.INFO,
@@ -160,8 +140,8 @@ gulp.task('connect', ['watch'], function () {
     }
   });
 
-  const debugLoggingOfProxy = process.env.DEBUG_PROXY === "true";
-  const useAuthentication   = process.env.DISABLE_OAUTH !== "true";
+  const debugLoggingOfProxy = process.env.DEBUG_PROXY === 'true';
+  const useAuthentication   = process.env.DISABLE_OAUTH !== 'true';
 
   const googleClientId     = process.env.GOOGLE_OAUTH_CLIENT_ID;
   const googleClientSecret = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
@@ -186,10 +166,10 @@ gulp.task('connect', ['watch'], function () {
       config.google     = {
         clientId         : googleClientId,
         clientSecret     : googleClientSecret,
-        authenticationURI: "https://accounts.google.com/o/oauth2/auth",
-        authorizationURI : "https://accounts.google.com/o/oauth2/auth",
-        scope            : "profile",
-        redirectURI      : "http://localhost:9000"
+        authenticationURI: 'https://accounts.google.com/o/oauth2/auth',
+        authorizationURI : 'https://accounts.google.com/o/oauth2/auth',
+        scope            : 'profile',
+        redirectURI      : 'http://localhost:9000'
       };
 
     } else if (useAuthentication) {
@@ -200,7 +180,7 @@ gulp.task('connect', ['watch'], function () {
         scope              : 'user:info user:check-access role:edit:hawtio'
       };
     }
-    const answer = "window.OPENSHIFT_CONFIG = window.HAWTIO_OAUTH_CONFIG = " + stringifyObject(config);
+    const answer = 'window.OPENSHIFT_CONFIG = window.HAWTIO_OAUTH_CONFIG = ' + stringifyObject(config);
     res.set('Content-Type', 'application/javascript');
     res.send(answer);
   });
@@ -209,11 +189,10 @@ gulp.task('connect', ['watch'], function () {
     const path = req.originalUrl;
     // avoid returning these files, they should get pulled from js
     if (s.startsWith(path, '/plugins/') && s.endsWith(path, 'html')) {
-      console.log("returning 404 for: ", path);
+      console.log('returning 404 for: ', path);
       res.statusCode = 404;
       res.end();
     } else {
-      //console.log("allowing: ", path);
       next();
     }
   });
@@ -233,79 +212,52 @@ gulp.task('connect', ['watch'], function () {
     }
   });
 
-  hawtio.listen(function (server) {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("started from gulp file at ", host, ":", port);
-  });
+  hawtio.listen(server => console.log('started from gulp file at ', server.address().address, ':', server.address().port));
 });
 
-gulp.task('reload', function () {
-  gulp.src('.')
-    .pipe(hawtio.reload());
-});
+gulp.task('reload', () => gulp.src('.').pipe(hawtio.reload()));
 
-gulp.task('site-fonts', function () {
-  return gulp.src(['libs/**/*.woff', 'libs/**/*.woff2', 'libs/**/*.ttf', 'libs/**/fonts/*.eot', 'libs/**/fonts/*.svg'], {base: '.'})
-    .pipe(plugins.flatten())
-    .pipe(plugins.chmod(0o644))
-    .pipe(plugins.dedupe({same: false}))
-    .pipe(plugins.debug({title: 'site font files'}))
-    .pipe(gulp.dest('site/fonts/', {overwrite: false}));
-});
+gulp.task('site-fonts', () => gulp.src(['libs/**/*.woff', 'libs/**/*.woff2', 'libs/**/*.ttf', 'libs/**/fonts/*.eot', 'libs/**/fonts/*.svg'], {base: '.'})
+  .pipe(plugins.flatten())
+  .pipe(plugins.chmod(0o644))
+  .pipe(plugins.dedupe({same: false}))
+  .pipe(plugins.debug({title: 'site font files'}))
+  .pipe(gulp.dest('site/fonts/', {overwrite: false})));
 
-gulp.task('root-files', function () {
-  return gulp.src(['favicon.ico'], {base: '.'})
-    .pipe(plugins.flatten())
-    .pipe(plugins.debug({title: 'root files'}))
-    .pipe(plugins.chmod(0o644))
-    .pipe(gulp.dest('site'));
-});
+gulp.task('root-files', () => gulp.src(['favicon.ico'], {base: '.'})
+  .pipe(plugins.flatten())
+  .pipe(plugins.debug({title: 'root files'}))
+  .pipe(plugins.chmod(0o644))
+  .pipe(gulp.dest('site')));
 
-gulp.task('site-files', function () {
-  return gulp.src(['images/**', 'img/**', 'osconsole/config.js'], {base: '.'})
-    .pipe(plugins.chmod(0o644))
-    .pipe(plugins.dedupe({same: false}))
-    .pipe(plugins.debug({title: 'site files'}))
-    .pipe(gulp.dest('site'));
-});
+gulp.task('site-files', () => gulp.src(['images/**', 'img/**', 'osconsole/config.js'], {base: '.'})
+  .pipe(plugins.chmod(0o644))
+  .pipe(plugins.dedupe({same: false}))
+  .pipe(plugins.debug({title: 'site files'}))
+  .pipe(gulp.dest('site')));
 
-gulp.task('usemin', function () {
-  return gulp.src('index.html')
-    .pipe(plugins.usemin({
-      css: [plugins.minifyCss({
-        keepBreaks: true
-      }), 'concat'],
-      js : [
-        plugins.uglify(),
-        plugins.rev()
-      ],
-      js1: [
-        plugins.uglify(),
-        plugins.rev()
-      ]
-    }))
-    .pipe(plugins.debug({title: 'usemin'}))
-    .pipe(gulp.dest('site'));
-});
+gulp.task('usemin', () => gulp.src('index.html')
+  .pipe(plugins.usemin({
+    css: [plugins.minifyCss({keepBreaks: true}), 'concat'],
+    js : [plugins.uglify(), plugins.rev()],
+    js1: [plugins.uglify(), plugins.rev()]
+  }))
+  .pipe(plugins.debug({title: 'usemin'}))
+  .pipe(gulp.dest('site')));
 
-gulp.task('tweak-urls', ['usemin'], function () {
-  return gulp.src('site/style.css')
-    .pipe(plugins.replace(/url\(\.\.\//g, 'url('))
-    // tweak fonts URL coming from PatternFly that does not repackage then in dist
-    .pipe(plugins.replace(/url\(\.\.\/components\/font-awesome\//g, 'url('))
-    .pipe(plugins.replace(/url\(\.\.\/components\/bootstrap\/dist\//g, 'url('))
-    .pipe(plugins.replace(/url\(libs\/bootstrap\/dist\//g, 'url('))
-    .pipe(plugins.replace(/url\(libs\/patternfly\/components\/bootstrap\/dist\//g, 'url('))
-    .pipe(plugins.debug({title: 'tweak-urls'}))
-    .pipe(gulp.dest('site'));
-});
+gulp.task('tweak-urls', ['usemin'], () => gulp.src('site/style.css')
+  .pipe(plugins.replace(/url\(\.\.\//g, 'url('))
+  // tweak fonts URL coming from PatternFly that does not repackage then in dist
+  .pipe(plugins.replace(/url\(\.\.\/components\/font-awesome\//g, 'url('))
+  .pipe(plugins.replace(/url\(\.\.\/components\/bootstrap\/dist\//g, 'url('))
+  .pipe(plugins.replace(/url\(libs\/bootstrap\/dist\//g, 'url('))
+  .pipe(plugins.replace(/url\(libs\/patternfly\/components\/bootstrap\/dist\//g, 'url('))
+  .pipe(plugins.debug({title: 'tweak-urls'}))
+  .pipe(gulp.dest('site')));
 
-gulp.task('404', ['usemin'], function () {
-  return gulp.src('site/index.html')
-    .pipe(plugins.rename('404.html'))
-    .pipe(gulp.dest('site'));
-});
+gulp.task('404', ['usemin'], () => gulp.src('site/index.html')
+  .pipe(plugins.rename('404.html'))
+  .pipe(gulp.dest('site')));
 
 gulp.task('copy-images', function () {
   const dirs     = fs.readdirSync('./libs');
@@ -330,16 +282,14 @@ gulp.task('copy-images', function () {
     .pipe(gulp.dest('site/img'));
 });
 
-gulp.task('collect-dep-versions', function () {
-  return gulp.src('./libs/**/.bower.json')
-    .pipe(plugins.foreach(function (stream, file) {
-      const pkg                = JSON.parse(file.contents.toString('utf8'));
-      bower.packages[pkg.name] = {
-        version: pkg.version
-      };
-      return stream;
-    }));
-});
+gulp.task('collect-dep-versions', () => gulp.src('./libs/**/.bower.json')
+  .pipe(plugins.foreach(function (stream, file) {
+    const pkg                = JSON.parse(file.contents.toString('utf8'));
+    bower.packages[pkg.name] = {
+      version: pkg.version
+    };
+    return stream;
+  })));
 
 gulp.task('get-commit-id', function (cb) {
   plugins.git.exec({args: 'rev-parse HEAD'}, function (err, stdout) {
@@ -378,11 +328,7 @@ gulp.task('serve-site', function () {
       enabled: false
     }
   });
-  return hawtio.listen(function (server) {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("started from gulp file at ", host, ":", port);
-  });
+  return hawtio.listen(server => console.log('started from gulp file at ', server.address().address, ':', server.address().port));
 });
 
 function configStaticAssets(prefix) {
@@ -396,9 +342,9 @@ function configStaticAssets(prefix) {
       const dirs = fs.readdirSync(targetDir);
       dirs.forEach(function (dir) {
         dir = urljoin(prefix, 'libs', dir);
-        console.log("dir: ", dir);
+        console.log('dir: ', dir);
         if (fs.statSync(dir).isDirectory()) {
-          console.log("Adding directory to search path: ", dir);
+          console.log('Adding directory to search path: ', dir);
           staticAssets.push({
             path: '/',
             dir : dir
@@ -407,7 +353,7 @@ function configStaticAssets(prefix) {
       });
     }
   } catch (err) {
-    console.log("Nothing in libs to worry about");
+    console.log('Nothing in libs to worry about');
   }
   return staticAssets;
 }
