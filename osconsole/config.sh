1f2a672d9be0d6ee299426aa5470d0fb3bffdf20
Antonin Stefanutti <antonin@stefanutti.fr>
2018-01-10 18:08:19 +0100

    Parameterized service account namespace

--- osconsole/config.sh
+++ osconsole/config.sh
@@ -4,8 +4,8 @@ cat << EOF
 window.OPENSHIFT_CONFIG = window.HAWTIO_OAUTH_CONFIG = {
   openshift: {
     oauth_authorize_uri: '${OPENSHIFT_MASTER}/oauth/authorize',
-    oauth_client_id: 'system:serviceaccount:hawtio:hawtio-oauth-client',
-    scope: 'user:info user:check-access role:edit:hawtio'
+    oauth_client_id: 'system:serviceaccount:${HAWTIO_NAMESPACE}:hawtio-oauth-client',
+    scope: 'user:info user:check-access role:edit:${HAWTIO_NAMESPACE}'
   }
 };
 EOF
