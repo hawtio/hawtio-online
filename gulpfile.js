e9f579f55ff29798789b4001a72d985632dae2c8
Antonin Stefanutti <antonin@stefanutti.fr>
2018-01-18 17:16:01 +0100

    Fix image URLs in CSS stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -247,7 +247,7 @@ gulp.task('usemin-online', () => gulp.src('online.html')
   .pipe(gulp.dest('site/online')));
 
 gulp.task('tweak-urls-integration', ['usemin-integration'], () => gulp.src('site/integration/style.css')
-  // .pipe(plugins.replace(/url\(\.\.\//g, 'url('))
+  .pipe(plugins.replace(/url\(img\//g, 'url(../img/'))
   // tweak fonts URL coming from PatternFly that does not repackage then in dist
   .pipe(plugins.replace(/url\(\.\.\/components\/font-awesome\//g, 'url(../'))
   .pipe(plugins.replace(/url\(\.\.\/components\/bootstrap\/dist\//g, 'url(../'))
@@ -257,7 +257,7 @@ gulp.task('tweak-urls-integration', ['usemin-integration'], () => gulp.src('site
   .pipe(gulp.dest('site/integration')));
 
 gulp.task('tweak-urls-online', ['usemin-online'], () => gulp.src('site/online/style.css')
-  // .pipe(plugins.replace(/url\(\.\.\//g, 'url('))
+  .pipe(plugins.replace(/url\(img\//g, 'url(../img/'))
   // tweak fonts URL coming from PatternFly that does not repackage then in dist
   .pipe(plugins.replace(/url\(\.\.\/components\/font-awesome\//g, 'url(../'))
   .pipe(plugins.replace(/url\(\.\.\/components\/bootstrap\/dist\//g, 'url(../'))
