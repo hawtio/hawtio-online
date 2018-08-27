bb0cf94cbf9f44d31d687c940cc51e65bcfe187e
Antonin Stefanutti <antonin@stefanutti.fr>
2018-08-27 11:18:16 +0200

    chore(navigation): disable Connect plugin navigation entry

--- packages/integration/src/app.module.ts
+++ packages/integration/src/app.module.ts
@@ -6,9 +6,22 @@ namespace Online {
     .module('hawtio-online-integration', [
       'hawtio-online-integration-navigation',
     ])
+    .decorator('mainNavService', disableConnectPlugin)
     .component('hawtioIntegrationApp', appComponent)
     .run(addLogoutToUserDropdown)
     .run(destroyBeforeUnload);
+
+  function disableConnectPlugin($delegate: Nav.MainNavService) {
+    'ngInject';
+    const addItem = $delegate.addItem;
+    $delegate.addItem = (item: Nav.MainNavItemProps): void => {
+      if (item.title === 'Connect') {
+        item.isValid = () => false;
+      }
+      addItem.call($delegate, item);
+    }
+    return $delegate;
+  }
 
   function addLogoutToUserDropdown(
     HawtioExtension: Core.HawtioExtension,
