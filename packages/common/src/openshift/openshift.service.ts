27a20271dbb0963bb40830622a1d18128ff7b68d
Antonin Stefanutti <antonin@stefanutti.fr>
2018-09-04 12:13:29 +0200

    feat(navigation): Make pod selector working in cluster mode

--- packages/common/src/openshift/openshift.service.ts
+++ packages/common/src/openshift/openshift.service.ts
@@ -3,8 +3,11 @@ namespace Online {
   export class OpenShiftService {
 
     private _loading = 0;
+    private projects = [];
     private pods = [];
+    private projects_client;
     private pods_client;
+    private projects_watch;
     private pods_watch;
 
     constructor(
@@ -14,19 +17,53 @@ namespace Online {
       'ngInject';
 
       if (this.$window.OPENSHIFT_CONFIG.hawtio.mode === 'cluster') {
-        // TODO
-        return;
+        this.projects_client = this.K8SClientFactory.create('projects');
+        const pods_watches = {};
+        this._loading++;
+        this.projects_watch = this.projects_client.watch(projects => {
+          // subscribe to pods update for new projects
+          projects.filter(project => !this.projects.some(p => p.metadata.uid === project.metadata.uid))
+            .forEach(project => {
+              this._loading++;
+              const pods = this.K8SClientFactory.create('pods', project.metadata.name);
+              const pods_watch = pods.watch(pods => {
+                this._loading--;
+                const others = this.pods.filter(pod => pod.metadata.namespace !== project.metadata.name);
+                this.pods.length = 0;
+                this.pods.push(...others, ..._.filter(pods, pod => jsonpath.query(pod, '$.spec.containers[*].ports[?(@.name=="jolokia")]').length > 0));
+              });
+              pods_watches[project.metadata.name] = {
+                request : pods,
+                watch   : pods_watch,
+              };
+              pods.connect();
+            });
+
+          // handle delete projects
+          this.projects.filter(project => !projects.some(p => p.metadata.uid === project.metadata.uid))
+            .forEach(project => {
+              const handle = pods_watches[project.metadata.name];
+              this.K8SClientFactory.destroy(handle.request, handle.watch);
+              delete pods_watches[project.metadata.name];
+            });
+
+          this.projects.length = 0;
+          this.projects.push(...projects);
+          this._loading--;
+        });
+
+        this.projects_client.connect();
+      } else {
+        this._loading++;
+        this.pods_client = this.K8SClientFactory.create('pods', this.$window.OPENSHIFT_CONFIG.hawtio.namespace);
+        this.pods_watch = this.pods_client.watch(pods => {
+          this._loading--;
+          this.pods.length = 0;
+          this.pods.push(..._.filter(pods, pod => jsonpath.query(pod, '$.spec.containers[*].ports[?(@.name=="jolokia")]').length > 0));
+        });
+
+        this.pods_client.connect();
       }
-
-      this._loading++;
-      this.pods_client = this.K8SClientFactory.create('pods', this.$window.OPENSHIFT_CONFIG.hawtio.namespace);
-      this.pods_watch = this.pods_client.watch(pods => {
-        this._loading--;
-        this.pods.length = 0;
-        this.pods.push(..._.filter(pods, pod => jsonpath.query(pod, '$.spec.containers[*].ports[?(@.name=="jolokia")]').length > 0));
-      });
-
-      this.pods_client.connect();
     }
 
     isLoading() {
@@ -38,6 +75,7 @@ namespace Online {
     }
 
     disconnect() {
+      this.K8SClientFactory.destroy(this.projects_client, this.projects_watch);
       this.K8SClientFactory.destroy(this.pods_client, this.pods_watch);
     }
   }
