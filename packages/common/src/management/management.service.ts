85d7aa0b4622e9dceffeed65029def48e0b80da7
Antonin Stefanutti <antonin@stefanutti.fr>
2018-09-14 17:39:29 +0200

    chore(management): Make sure pod reference is up-to-date

--- packages/common/src/management/management.service.ts
+++ packages/common/src/management/management.service.ts
@@ -24,13 +24,15 @@ namespace Online {
       openShiftService.on('changed', _ => {
         const pods = openShiftService.getPods();
         openShiftService.getPods().forEach(pod => {
-          if (!this.pods[pod.metadata.uid]) {
+          const mPod = this.pods[pod.metadata.uid];
+          if (!mPod) {
             // FIXME: read Jolokia port from container spec
             const port = 8778;
             const url = new URI().query('').path(`/master/api/v1/namespaces/${pod.metadata.namespace}/pods/https:${pod.metadata.name}:${port}/proxy/jolokia/`)
             this.pods[pod.metadata.uid] = new ManagedPod(pod, new Jolokia(url.valueOf()));
           } else {
-            pod.management = this.pods[pod.metadata.uid].pod.management;
+            pod.management = mPod.pod.management;
+            mPod.pod = pod;
           }
           for (let uid in this.pods) {
             if (!pods.some(pod => pod.metadata.uid === uid)) {
