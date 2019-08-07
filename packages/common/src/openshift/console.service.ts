917a849d14a9b6e5aa9f340743443f7f849e297c
Antonin Stefanutti <antonin@stefanutti.fr>
2019-08-07 15:17:16 +0200

    Expose OpenShift Web console URL in OpenShift console service

--- packages/common/src/openshift/console.service.ts
+++ packages/common/src/openshift/console.service.ts
@@ -4,25 +4,34 @@ namespace Online {
 
   export class ConsoleService {
 
-    private _response: ng.IPromise<string|undefined>;
+    private _url: ng.IPromise<string | undefined>;
 
-    constructor($http: ng.IHttpService) {
+    constructor(
+      $http: ng.IHttpService,
+      $window: ng.IWindowService,
+      $q: ng.IQService,
+    ) {
       'ngInject';
-      this._response = $http({
-        method : 'GET',
-        url    : new URI().query('').path('/console').valueOf(),
-      }).then(response => {
-        const url = response.headers('location');
-        log.debug('Using OpenShift Web console URL:', url);
-        return response.headers('location');
-      }, error => {
-        log.debug('Unable to retrieve OpenShift Web console URL');
-        return undefined;
-      });
+      const url = $window.OPENSHIFT_CONFIG.openshift.web_console_url;
+      if (url) {
+        this._url = $q.resolve(url);
+      } else {
+        this._url = $http({
+          method : 'GET',
+          url    : new URI().query('').path('/console').valueOf(),
+        }).then(response => {
+          const url = response.headers('location');
+          log.debug('Using OpenShift Web console URL:', url);
+          return response.headers('location');
+        }, error => {
+          log.debug('Unable to retrieve OpenShift Web console URL');
+          return undefined;
+        });
+      }
     }
 
-    get url(): ng.IPromise<string|undefined> {
-      return this._response;
+    get url(): ng.IPromise<string | undefined> {
+      return this._url;
     }
   }
 }
