c9b7231c7980598163354c034a879b3158969ed2
Antonin Stefanutti <antonin@stefanutti.fr>
2019-08-07 15:15:14 +0200

    Expose OpenShift cluster version and Web console URL in OpenShift console config

--- docker/osconsole/config.sh
+++ docker/osconsole/config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "${HAWTIO_ONLINE_MODE}" = "cluster" ]; then 
+if [ "${HAWTIO_ONLINE_MODE}" = "cluster" ]; then
 cat << EOF
 window.OPENSHIFT_CONFIG = {
   master_uri: new URI().query('').path('/master').toString(),
@@ -10,7 +10,9 @@ window.OPENSHIFT_CONFIG = {
   openshift: {
     oauth_metadata_uri: new URI().query('').path('/master/.well-known/oauth-authorization-server').toString(),
     oauth_client_id: '${HAWTIO_OAUTH_CLIENT_ID:-hawtio-online}',
-    scope: 'user:info user:check-access user:list-projects role:edit:*'
+    scope: 'user:info user:check-access user:list-projects role:edit:*',
+    web_console_url: '${OPENSHIFT_WEB_CONSOLE_URL:-}',
+    cluster_version: '${OPENSHIFT_CLUSTER_VERSION:-}'
   }
 };
 EOF
@@ -26,7 +28,9 @@ window.OPENSHIFT_CONFIG = {
   openshift: {
     oauth_metadata_uri: new URI().query('').path('/master/.well-known/oauth-authorization-server').toString(),
     oauth_client_id: 'system:serviceaccount:${HAWTIO_ONLINE_NAMESPACE}:${HAWTIO_OAUTH_CLIENT_ID:-hawtio-online}',
-    scope: 'user:info user:check-access role:edit:${HAWTIO_ONLINE_NAMESPACE}'
+    scope: 'user:info user:check-access role:edit:${HAWTIO_ONLINE_NAMESPACE}',
+    web_console_url: '${OPENSHIFT_WEB_CONSOLE_URL:-}',
+    cluster_version: '${OPENSHIFT_CLUSTER_VERSION:-}'
   }
 };
 EOF
