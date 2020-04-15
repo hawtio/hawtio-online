7f6cf9685f796f2e99bde27c440ac612cbef4366
Antonin Stefanutti <antonin@stefanutti.fr>
2020-04-15 17:01:41 +0200

    chore(RBAC): Use promises instead of callbacks

--- docker/test.js
+++ docker/test.js
@@ -2,41 +2,69 @@
 // https://github.com/nginx/njs/issues/115
 import proxyJolokiaAgent from './nginx.js'
 
-proxyJolokiaAgent({
-  uri: '/management/namespaces/test/pods/https:pod:443/remaining',
-  requestBody: JSON.stringify({
-    mbean: 'org.apache.camel:type=context',
-    operation: 'dumpRoutesAsXml',
-  }),
-  headersOut: {},
-  subrequest: (uri, options, callback) => {
-    var body = JSON.parse(options.body || '{}');
-    var res;
-    if (uri.startsWith('/authorization') && body.verb === 'update') {
-      res = {
-        status: 201,
-        responseBody: JSON.stringify({
-          allowed: false,
-        }),
-      };
-    }
-    if (uri.startsWith('/authorization') && body.verb === 'get') {
-      res = {
-        status: 201,
-        responseBody: JSON.stringify({
-          allowed: true,
-        }),
-      };
-    }
-    if (!res) {
-      res = {
-        status: 404,
-        responseBody: `No stub for ${uri}`,
-      }
-    }
-    callback(Object.assign({ headersOut: {} }, res));
-  },
-  return: (code, message) => {
-    console.log('code:', code, 'message:', message);
-  },
-});
+function requestWithViewerRoleTest() {
+  proxyJolokiaAgent({
+    uri: '/management/namespaces/test/pods/https:pod:443/remaining',
+    requestBody: JSON.stringify({
+      mbean: 'org.apache.camel:type=context',
+      operation: 'dumpRoutesAsXml',
+    }),
+    headersOut: {},
+    subrequest: doWithViewerRole,
+    return: (code, message) => {
+      console.log('code:', code, 'message:', message);
+    },
+  });
+}
+
+function batchRequestWithViewerRoleTest() {
+  proxyJolokiaAgent({
+    uri: '/management/namespaces/test/pods/https:pod:443/remaining',
+    requestBody: JSON.stringify([
+      {
+        mbean: 'org.apache.camel:type=context',
+        operation: 'dumpRoutesAsXml',
+      },
+      {
+        mbean: 'java.lang.Memory',
+        operation: 'gc',
+      },
+    ]),
+    headersOut: {},
+    subrequest: doWithViewerRole,
+    return: (code, message) => {
+      console.log('code:', code, 'message:', message);
+    },
+  });
+}
+
+function doWithViewerRole(uri, options) {
+  var body = JSON.parse(options.body || '{}');
+  var res;
+  if (uri.startsWith('/authorization') && body.verb === 'update') {
+    res = {
+      status: 201,
+      responseBody: JSON.stringify({
+        allowed: false,
+      }),
+    };
+  }
+  if (uri.startsWith('/authorization') && body.verb === 'get') {
+    res = {
+      status: 201,
+      responseBody: JSON.stringify({
+        allowed: true,
+      }),
+    };
+  }
+  if (!res) {
+    return Promise.reject(Error(`No stub for ${uri}`));
+  }
+  Object.assign(res, {
+    headersOut: {},
+  });
+  return Promise.resolve(res);
+}
+
+requestWithViewerRoleTest();
+batchRequestWithViewerRoleTest();
