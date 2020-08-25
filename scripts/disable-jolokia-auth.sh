f7c8ae1816581f33a5c251fb163e2045d940cf5d
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2020-08-25 10:10:57 +0900

    refactor(dev): set env vars for dc in one shot

--- scripts/disable-jolokia-auth.sh
+++ scripts/disable-jolokia-auth.sh
@@ -12,12 +12,9 @@ fi
 for name in $names; do
   echo "Disabling: $name"
 
-  echo "  oc set env dc/$name AB_JOLOKIA_AUTH_OPENSHIFT=false"
-  oc set env dc/$name AB_JOLOKIA_AUTH_OPENSHIFT=false
-
-  echo "  oc set env dc/$name AB_JOLOKIA_PASSWORD_RANDOM=false"
-  oc set env dc/$name AB_JOLOKIA_PASSWORD_RANDOM=false
-
-  echo "  oc set env dc/$name AB_JOLOKIA_OPTS=useSslClientAuthentication=false,protocol=https"
-  oc set env dc/$name AB_JOLOKIA_OPTS=useSslClientAuthentication=false,protocol=https
+  echo "  oc set env dc/$name AB_JOLOKIA_AUTH_OPENSHIFT=false AB_JOLOKIA_PASSWORD_RANDOM=false AB_JOLOKIA_OPTS=useSslClientAuthentication=false,protocol=https"
+  oc set env dc/$name \
+    AB_JOLOKIA_AUTH_OPENSHIFT=false \
+    AB_JOLOKIA_PASSWORD_RANDOM=false \
+    AB_JOLOKIA_OPTS=useSslClientAuthentication=false,protocol=https
 done
