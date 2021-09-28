114527d47afeecceab73335ead47e775b1f24510
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-09-28 13:39:08 +0900

    feat(docker): ENTESB-16897 - provide nginx performance tuning via env vars

--- docker/nginx.sh
+++ docker/nginx.sh
@@ -8,12 +8,25 @@ set -eu
 
 ./config.sh > config.js
 
+# nginx.conf parameter default values
+export NGINX_SUBREQUEST_OUTPUT_BUFFER_SIZE=${NGINX_SUBREQUEST_OUTPUT_BUFFER_SIZE:-10m}
+export NGINX_CLIENT_BODY_BUFFER_SIZE=${NGINX_CLIENT_BODY_BUFFER_SIZE:-256k}
+export NGINX_PROXY_BUFFERS=${NGINX_PROXY_BUFFERS:-16 128k}
+
+generate_nginx_gateway_conf() {
+  envsubst '
+    $NGINX_SUBREQUEST_OUTPUT_BUFFER_SIZE
+    $NGINX_CLIENT_BODY_BUFFER_SIZE
+    $NGINX_PROXY_BUFFERS
+    ' < /nginx-gateway.conf.template > /etc/nginx/conf.d/nginx.conf
+}
+
 if [ -v HAWTIO_ONLINE_RBAC_ACL ]; then
   echo Using RBAC NGINX configuration
-  ln -sf /nginx-gateway.conf /etc/nginx/conf.d/nginx.conf
+  generate_nginx_gateway_conf
 elif [ "${HAWTIO_ONLINE_GATEWAY:-}" = "true" ]; then
   echo Using gateway NGINX configuration
-  ln -sf /nginx-gateway.conf /etc/nginx/conf.d/nginx.conf
+  generate_nginx_gateway_conf
 else
   echo Using legacy NGINX configuration
   ln -sf /nginx.conf /etc/nginx/conf.d/nginx.conf
