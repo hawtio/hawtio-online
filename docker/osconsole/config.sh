be207bcdc75454049f3935a770c9ec82a91f12f9
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-12-07 16:13:20 +0900

    feat(k8s): support HAWTIO_ONLINE_AUTH=form in osconsole/config.sh

--- docker/osconsole/config.sh
+++ docker/osconsole/config.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [ "${HAWTIO_ONLINE_MODE}" = "cluster" ]; then
-cat << EOF
+FORM_URI=/online/login.html
+
+openshift_config_cluster() {
+  cat << EOF
 window.OPENSHIFT_CONFIG = {
   master_uri: new URI().query('').path('/master').toString(),
   hawtio: {
@@ -16,9 +18,10 @@ window.OPENSHIFT_CONFIG = {
   }
 };
 EOF
-elif [ "${HAWTIO_ONLINE_MODE}" = "namespace" ]; then
-if [ -n "${HAWTIO_ONLINE_NAMESPACE:-}" ]; then
-cat << EOF
+}
+
+openshift_config_namespace() {
+  cat << EOF
 window.OPENSHIFT_CONFIG = {
   master_uri: new URI().query('').path('/master').toString(),
   hawtio: {
@@ -34,12 +37,70 @@ window.OPENSHIFT_CONFIG = {
   }
 };
 EOF
-else
-  >&2 echo HAWTIO_ONLINE_NAMESPACE must be set when HAWTIO_ONLINE_MODE=namespace
-  exit 1
-fi
-else
+}
+
+form_config_cluster() {
+  cat << EOF
+window.HAWTIO_OAUTH_CONFIG = {
+  master_uri: new URI().query('').path('/master').toString(),
+  hawtio: {
+    mode: '${HAWTIO_ONLINE_MODE}'
+  },
+  form: {
+    uri: new URI().query('').path('${FORM_URI}').toString()
+  }
+};
+EOF
+}
+
+form_config_namespace() {
+  cat << EOF
+window.HAWTIO_OAUTH_CONFIG = {
+  master_uri: new URI().query('').path('/master').toString(),
+  hawtio: {
+    mode: '${HAWTIO_ONLINE_MODE}',
+    namespace: '${HAWTIO_ONLINE_NAMESPACE}'
+  },
+  form: {
+    uri: new URI().query('').path('${FORM_URI}').toString()
+  }
+};
+EOF
+}
+
+cluster() {
+  case "${HAWTIO_ONLINE_AUTH}" in
+    "form") form_config_cluster;;
+    "oauth") openshift_config_cluster;;
+    *)
+      # fallback to OpenShift OAuth for backward compatibility
+      openshift_config_cluster;;
+  esac
+}
+
+namespace() {
+  if [ -z "${HAWTIO_ONLINE_NAMESPACE:-}" ]; then
+    >&2 echo HAWTIO_ONLINE_NAMESPACE must be set when HAWTIO_ONLINE_MODE=namespace
+    exit 1
+  fi
+
+  case "${HAWTIO_ONLINE_AUTH}" in
+    "form") form_config_namespace;;
+    "oauth") openshift_config_namespace;;
+    *)
+      # fallback to OpenShift OAuth for backward compatibility
+      openshift_config_namespace;;
+  esac
+}
+
+invalid() {
   >&2 echo Invalid value for the HAWTIO_ONLINE_MODE environment variable.
   >&2 echo It should either be \'cluster\' or \'namespace\', but is "${HAWTIO_ONLINE_MODE:-not set}".
   exit 1
-fi;
+}
+
+case "${HAWTIO_ONLINE_MODE}" in
+  "cluster") cluster;;
+  "namespace") namespace;;
+  *) invalid;;
+esac
