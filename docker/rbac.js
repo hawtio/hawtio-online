4dd030b354475d0ff32e74e19f78549698010673
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-03-25 11:21:09 +0900

    feat(docker): implement RBACRegistry in hawtio-online image
    It will optimise the initial communication between client and nginx
    server from several megabytes of MBeans list data to a few hundreds
    kilobytes for a large number of MBeans such as a Camel app with 500
    routes.

--- docker/rbac.js
+++ docker/rbac.js
@@ -2,14 +2,20 @@ var ACL = ''; // Load ACL from nginx.js first
 var regex = /^\/.*\/$/;
 var rbacSearchKeyword = '*:type=security,area=jmx,*';
 var rbacMBean = 'hawtio:type=security,area=jmx,name=HawtioOnlineRBAC';
+var rbacRegistryPath = 'hawtio/type=security,name=RBACRegistry';
+var rbacRegistryMBean = 'hawtio:type=security,name=RBACRegistry';
 
 // Expose private functions for testing
-var testing = { isSearchRBACMBean, isBulkCanInvokeRequest };
+var testing = { optimisedMBeans, identifySpecialMBean, getProperty };
 
-export default { initACL, check, intercept, isCanInvokeRequest, testing };
+export default { initACL, check, intercept, isMBeanListRequired, testing };
 
 function initACL(acl) {
   ACL = acl;
+}
+
+function isMBeanListRequired(request) {
+  return isCanInvokeRequest(request) || isExecRBACRegistryList(request);
 }
 
 function isSearchRBACMBean(request) {
@@ -23,6 +29,16 @@ function isCanInvokeRequest(request) {
 function isBulkCanInvokeRequest(request) {
   return request.type === 'exec' && request.mbean === rbacMBean && request.operation === 'canInvoke(java.util.Map)';
 }
+
+function isListRBACRegistry(request) {
+  return request.type === 'list' && request.path === rbacRegistryPath;
+}
+
+function isExecRBACRegistryList(request) {
+  return request.type === 'exec' && request.mbean === rbacRegistryMBean && request.operation === 'list()';
+}
+
+// ===== intercept =========================================
 
 function intercept(request, role, mbeans) {
   var intercepted = value => ({
@@ -48,13 +64,15 @@ function intercept(request, role, mbeans) {
     var domain = i === -1 ? mbean : mbean.substring(0, i);
     var properties = mbean.substring(i + 1);
 
-    var infos = (mbeans[domain] || {})[properties];
-    if (!infos) {
+    // MBeanInfo
+    // https://docs.oracle.com/en/java/javase/11/docs/api/java.management/javax/management/MBeanInfo.html
+    var info = (mbeans[domain] || {})[properties];
+    if (!info) {
       return intercepted(false);
     }
 
     // Check operations
-    var res = Object.entries(infos.op || [])
+    var res = Object.entries(info.op || [])
       // handle overloaded methods
       .map(op => Array.isArray(op[1]) ? op[1].map(o => [op[0], o]) : [op])
       // flatMap shim
@@ -67,7 +85,7 @@ function intercept(request, role, mbeans) {
     }
 
     // Check attributes
-    res = Object.entries(infos.attr || [])
+    res = Object.entries(info.attr || [])
       .find(attr => {
         var name = attr[0];
         var type = attr[1].type;
@@ -99,11 +117,131 @@ function intercept(request, role, mbeans) {
     return intercepted(value);
   }
 
+  // Intercept client-side RBACRegistry discovery request
+  if (isListRBACRegistry(request)) {
+    return intercepted({
+      class: 'io.hawt.jmx.RBACRegistry',
+      desc: 'Hawtio Online RBACRegistry',
+      op: {
+        list: {
+          desc: 'Hawtio Online RBACRegistry - list',
+          args: [],
+          ret: 'java.util.Map',
+        }
+      },
+    });
+  }
+
+  // Intercept client-side optimised list MBeans request
+  if (isExecRBACRegistryList(request)) {
+    return intercepted(optimisedMBeans(mbeans));
+  }
+
   return {
     intercepted: false,
     request: request,
   };
 }
+
+function optimisedMBeans(mbeans) {
+  // domain -> [mbean, mbean, ...], where mbean is either inline jsonified MBeanInfo
+  // or a key to shared jsonified MBeanInfo
+  var domains = {};
+  // if MBean is found to be "special", we can cache JSONified MBeanInfo
+  // (an object with "op", "attr" and "desc" properties)
+  // key -> [mbeaninfo, mbeaninfo, ...]
+  var cache = {};
+
+  var visited = {};
+  Object.entries(mbeans)
+    // convert MBean tree to an array of [domain, properties, MBeanInfo]
+    .map(infos => Object.entries(infos[1]).map(info => [infos[0], info[0], info[1]]))
+    .reduce((acc, infos) => { infos.forEach(i => acc.push(i)); return acc; }, [])
+    // process each MBeanInfo
+    .forEach(info => addMBeanInfo(cache, domains, visited, info[0], info[1], info[2]));
+
+  // TODO: try adding RBAC info
+  // tryAddRBACInfo(domains, cache);
+
+  return {
+    cache: cache,
+    domains: domains,
+  };
+}
+
+function addMBeanInfo(cache, domains, visited, domain, props, mbeanInfo) {
+  var objectName = `${domain}:${props}`;
+  if (visited[objectName]) {
+    return;
+  }
+
+  var cacheKey = identifySpecialMBean(domain, props, mbeanInfo);
+  if (cacheKey && !cache[cacheKey]) {
+    cache[cacheKey] = mbeanInfo;
+  }
+
+  if (!domains[domain]) {
+    domains[domain] = {};
+  }
+  if (cacheKey) {
+    domains[domain][props] = cacheKey;
+  } else {
+    domains[domain][props] = mbeanInfo;
+  }
+
+  visited[objectName] = true;
+}
+
+/*
+ * If the ObjectName is detected as special (when we may have thousands of such MBeans),
+ * we return a key to lookup already processed MBeanInfo.
+ * 
+ * If some combination of ObjectName and MBean's class name is detected as special,
+ * we may cache the MBeanInfo as well.
+ */
+function identifySpecialMBean(domain, props, mbeanInfo) {
+  switch (domain) {
+    case 'org.apache.activemq':
+      var destType = getProperty(props, 'destinationType');
+      // see: org.apache.activemq.command.ActiveMQDestination.getDestinationTypeAsString()
+      switch (destType) {
+        case 'Queue':
+          return 'activemq:queue';
+        case 'TempQueue':
+          return 'activemq:tempqueue';
+        case 'Topic':
+          return 'activemq:topic';
+        case 'TempTopic':
+          return 'activemq:temptopic';
+      }
+      break;
+    case 'org.apache.activemq.artemis':
+      var comp = getProperty(props, 'component');
+      if (comp === 'addresses') {
+        var subcomp = getProperty(props, 'subcomponent');
+        if (subcomp === null) {
+          return 'activemq.artemis:address';
+        } else if (subcomp === 'queues') {
+          return 'activemq.artemis:queue';
+        }
+      }
+      break;
+    case 'org.apache.camel':
+      // "type" attribute is not enough - we have to know real class of MBean
+      return `camel::${mbeanInfo.class}`;
+  }
+
+  return null;
+}
+
+function getProperty(props, name) {
+  var prop = props.split(',')
+    .map(prop => prop.split('='))
+    .find(prop => prop[0] === name);
+  return prop ? prop[1] : null;
+}
+
+// ===== check =============================================
 
 function check(request, role) {
   var mbean = request.mbean;
