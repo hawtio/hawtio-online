fe51889e6699d7ac25ef2c975e52c5b1419aebdf
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2023-06-30 13:20:58 +0100

    chore: Starts to implement testing of kubernetes-api package
    * Get some confidence in ensuring package is working as intended by getting
      some tests implements for kubernetes-api package

--- packages/kubernetes-api/jest.config.ts
+++ packages/kubernetes-api/jest.config.ts
@@ -8,17 +8,22 @@ export default {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
+  // Mocks necessary for dealing with jest issues, eg.
+  // "SyntaxError: Cannot use import statement outside a module"
+  // can happen if the react-code-editor entry here is removed.
   moduleNameMapper: {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
-  //     '<rootDir>/src/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/plugins/__mocks__/styleMock.js',
     'react-markdown': '<rootDir>/../../node_modules/react-markdown/react-markdown.min.js',
-  //   '@patternfly/react-code-editor': path.resolve(__dirname, './src/__mocks__/codeEditorMock.js'),
+    '@patternfly/react-code-editor': path.resolve(__dirname, './plugins/__mocks__/codeEditorMock.js'),
   },
 
   // The path to a module that runs some code to configure or set up the testing
   // framework before each test
-  // setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  //
+  // Necessary to avoid the error message "ReferenceError: jQuery is not defined"
+  setupFilesAfterEnv: ['<rootDir>/plugins/setupTests.ts'],
 
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '../../node_modules'],
 
--- packages/kubernetes-api/plugins/kubernetes-api-helpers.test.ts
+++ packages/kubernetes-api/plugins/kubernetes-api-helpers.test.ts
@@ -1,15 +1,56 @@
 import * as helpers from './kubernetes-api-helpers'
-import Logger from 'js-logger'
-
-const mockTestLogger = Logger.get('test-logger')
-
-jest.mock('@hawtio/react', () => ({
-  Logger: () => mockTestLogger
-}))
+import { WatchTypes } from './kubernetes-api-model'
+import { OS_PREFIX, OS_API_VERSION, K8S_PREFIX, K8S_API_VERSION, K8S_EXT_PREFIX, K8S_EXT_VERSION } from './kubernetes-api-globals'
 
 describe('arrays', () => {
   test('masterApiUrl', () => {
     const result = helpers.masterApiUrl()
     expect(result).toEqual("")
+  })
+
+  test('namespaced', () => {
+    expect(helpers.namespaced(WatchTypes.LIST)).toBeTruthy()
+    expect(helpers.namespaced(WatchTypes.ENDPOINTS)).toBeTruthy()
+    expect(helpers.namespaced(WatchTypes.EVENTS)).toBeTruthy()
+
+    expect(helpers.namespaced(WatchTypes.POLICIES)).toBeFalsy()
+    expect(helpers.namespaced(WatchTypes.OAUTH_CLIENTS)).toBeFalsy()
+    expect(helpers.namespaced(WatchTypes.NAMESPACES)).toBeFalsy()
+    expect(helpers.namespaced(WatchTypes.NODES)).toBeFalsy()
+    expect(helpers.namespaced(WatchTypes.PERSISTENT_VOLUMES)).toBeFalsy()
+    expect(helpers.namespaced(WatchTypes.PROJECTS)).toBeFalsy()
+  })
+
+  test('prefixes', () => {
+    expect(helpers.kubernetesApiPrefix()).toBe(K8S_PREFIX + '/' + K8S_API_VERSION)
+    expect(helpers.kubernetesApiExtensionPrefix()).toBe('apis/extensions/v1beta1')
+
+    expect(helpers.openshiftApiPrefix('')).toBe(OS_PREFIX + '/' + OS_API_VERSION)
+    expect(helpers.openshiftApiPrefix(WatchTypes.NODES)).toBe(OS_PREFIX + '/' + OS_API_VERSION)
+    expect(helpers.openshiftApiPrefix(WatchTypes.BUILD_CONFIGS)).toBe(OS_PREFIX + '/build.openshift.io/' + OS_API_VERSION)
+
+    expect(helpers.prefixForKind('')).toBeNull()
+    expect(helpers.prefixForKind(WatchTypes.NAMESPACES)).toBe(K8S_PREFIX + '/' + K8S_API_VERSION)
+    expect(helpers.prefixForKind(WatchTypes.DEPLOYMENTS)).toBe(K8S_EXT_PREFIX + '/' + K8S_EXT_VERSION)
+    expect(helpers.prefixForKind(WatchTypes.CONFIG_MAPS)).toBe(K8S_PREFIX + '/' + K8S_API_VERSION)
+    expect(helpers.prefixForKind(WatchTypes.IMAGE_STREAMS)).toBe(OS_PREFIX + '/' + OS_API_VERSION)
+  })
+
+  test('wsscheme', () => {
+    expect(helpers.wsScheme('http://192.168.58.2:8443/apis/apps/v1/deployments')).toBe('ws')
+    expect(helpers.wsScheme('https://192.168.58.2:8443/apis/apps/v1/deployments')).toBe('wss')
+  })
+
+  test('createShallowObject', () => {
+    const expected = {
+      apiVersion: "v1",
+      kind: 'deployment',
+      metadata: {
+        name: 'test',
+        namespace: 'scratch'
+      },
+    }
+
+    expect(helpers.createShallowObject('test', 'deployment', 'scratch')).toEqual(expected)
   })
 })
