9451429f3a7245562426f39ba8661eb69a1a5411
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2023-09-22 14:12:27 +0100

    fix: Includes manifest and logo in compiled build
    * Uses CopyWebpackPlugin to include non-referenced items into served build

--- packages/online-shell/webpack.config.prod.js
+++ packages/online-shell/webpack.config.prod.js
@@ -1,10 +1,20 @@
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 const { common } = require('./webpack.config.common.js')
 
 module.exports = () => {
   return merge(common('production'), {
-    devtool: 'source-map'
+    devtool: 'source-map',
+
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public/manifest.json' },
+          { from: 'public/hawtio-logo.svg'}
+        ]
+      })
+    ]
   })
 }
