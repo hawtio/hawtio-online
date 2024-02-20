c46c85c600c39061dc121b5f5eb098a48a036b4e
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-02-20 16:06:16 +0000

    fix: migrate configuration of webpack-dev-server for version 5
    * Replaces deprecated and now removed 'onBeforeSetupMiddleware
    
    * Migrates proxy configuration syntax

--- packages/management-api-app/webpack.config.js
+++ packages/management-api-app/webpack.config.js
@@ -183,20 +183,25 @@ module.exports = () => {
        *       thrown by the proxy but with an incorrect error message with
        *       the original host address in it rather than the target
        */
-      proxy: {
-        '/master': {
+      proxy: [
+        {
+          context: ['/master'],
           target: master_uri,
           pathRewrite: { '^/master': '' },
           secure: false,
           ws: true,
         },
-      },
+      ],
 
       static: {
         directory: path.join(__dirname, 'public'),
       },
 
       setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error('webpack-dev-server is not defined')
+        }
+
         /*
          * Function to construct the config.json file
          * and make it available for authentication
