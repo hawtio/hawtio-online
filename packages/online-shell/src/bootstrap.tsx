a57f15c145466e54265b0b9100556434a1eeb2b3
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-03-06 13:40:01 +0000

    chore: Replaces registerPlugins function with individual plugin init calls
    * Avoids need to disable the connect plugin since we no longer have to
      register it in the first place since we can control which plugins should
      be included

--- packages/online-shell/src/bootstrap.tsx
+++ packages/online-shell/src/bootstrap.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configManager, hawtio, Hawtio, registerPlugins } from '@hawtio/react'
+import { camel, configManager, hawtio, Hawtio, jmx, logs, quartz, rbac, runtime, springboot } from '@hawtio/react'
 import { isMgmtApiRegistered } from '@hawtio/online-management-api'
 import { reportWebVitals } from './reportWebVitals'
 import { discover } from './discover'
-import { log } from './discover/globals'
 
 configManager.addProductInfo('Hawtio Online', '__PACKAGE_VERSION_PLACEHOLDER__')
 
 // Register kubernetes & management - only then complete hawtio bootstrap
 isMgmtApiRegistered().then(() => {
   // Register hawtio plugins
-  registerPlugins()
-
-  // Disable the connect plugin
-  for (const plugin of hawtio.getPlugins()) {
-    if (plugin.id === 'connect') {
-      log.debug('Disabling connect plugin')
-      plugin.isActive = async () => {
-        return false
-      }
-    }
-  }
+  jmx()
+  rbac()
+  camel()
+  runtime()
+  logs()
+  quartz()
+  springboot()
 
   // Register discover plugin
   discover()
