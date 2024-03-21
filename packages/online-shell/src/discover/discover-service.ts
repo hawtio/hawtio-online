9483d3c16a2ce81b2e14387d44f0639b46918aad
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-03-21 13:51:40 +0000

    fix: Stops infinite loop in Discover context useEffect
    * context.ts
     * No longer reads and sets discoverGroups and discoverPods in the
       organisePods function. Both are only created / modified in the discover
       service
     * organisePods function is coverted to a react callback that is only
       updated on the change in the filters
    
    * discover-service.ts
     * Converts to a class in order to preserve the state of the discoverGroups
       and discoverPods so they are available to be fed back into the
       filterAndGroupPods() function as before
    
    * DiscoverToolbar.tsx
     * Only updates the filters and leaves the updating of the groups and pods
       to the context useEffect()

--- packages/online-shell/src/discover/discover-service.ts
+++ packages/online-shell/src/discover/discover-service.ts
@@ -2,156 +2,172 @@ import { OwnerReference } from '@hawtio/online-kubernetes-api'
 import { ManagedPod, mgmtService } from '@hawtio/online-management-api'
 import { DiscoverGroup, DiscoverPod, DiscoverType, TypeFilter } from './globals'
 
-export function unwrap(error: Error): string {
-  if (!error) return 'unknown error'
-
-  if (error.cause instanceof Error) return unwrap(error.cause)
-
-  return error.message
-}
-
-function applyFilter(filter: TypeFilter, pod: ManagedPod): boolean {
-  if (!pod.getMetadata()) return false
-
-  type KubeObjKey = keyof typeof pod.getMetadata
-
-  const podProp = pod.getMetadata[filter.type.toLowerCase() as KubeObjKey] as string
-
-  // Want to filter on this property but value
-  // is null so filter fails
-  if (!podProp) return false
-
-  return podProp.toLowerCase().includes(filter.value.toLowerCase())
-}
-
-function recordValue(values: Record<string, string> | undefined, key: string): string | undefined {
-  if (!values) return undefined
-  return values[key]
-}
-
-function toDiscoverGroup(
-  pod: ManagedPod,
-  owner: OwnerReference,
-  replicas: ManagedPod[],
-  expanded: boolean,
-): DiscoverGroup {
-  const discoverReplicas = [pod, ...replicas].map(replica => createDiscoverPod(replica))
-
-  return {
-    type: DiscoverType.Group,
-    name: owner.name,
-    uid: owner.uid,
-    namespace: pod.getMetadata()?.namespace || '<unknown>',
-    replicas: discoverReplicas || [],
-    expanded: expanded,
-    config: recordValue(pod.getMetadata()?.annotations, 'openshift.io/deployment-config.name'),
-    version: recordValue(pod.getMetadata()?.annotations, 'openshift.io/deployment-config.latest-version'),
-    statefulset: recordValue(pod.getMetadata()?.labels, 'statefulset.kubernetes.io/pod-name'),
-  }
-}
-
-function createDiscoverPod(pod: ManagedPod): DiscoverPod {
-  return {
-    type: DiscoverType.Pod,
-    name: pod.getMetadata()?.name || '<unknown>',
-    uid: pod.getMetadata()?.uid || '<unknown>',
-    namespace: pod.getMetadata()?.namespace || '<unknown>',
-    labels: pod.getMetadata()?.labels || {},
-    annotations: pod.getMetadata()?.annotations || {},
-    mPod: pod,
-  }
-}
-
-function podOwner(pod: ManagedPod): OwnerReference | null {
-  const metadata = pod.getMetadata()
-  if (!metadata || !metadata.ownerReferences) return null
-
-  if (metadata.ownerReferences.length === 0) return null
-
-  return metadata.ownerReferences[0]
-}
-
-function podsWithOwner(remainingPods: ManagedPod[], ownerRef: OwnerReference): ManagedPod[] {
-  if (!remainingPods) return []
-
-  return remainingPods.filter(pod => {
-    const oRef = podOwner(pod)
-    return oRef?.uid === ownerRef.uid
-  })
-}
-
-function groupPodsByDeployment(
-  pods: ManagedPod[],
-  exDiscoverGroups: DiscoverGroup[],
-): [DiscoverGroup[], DiscoverPod[]] {
-  const discoverPods: DiscoverPod[] = []
-  const discoverGroups: DiscoverGroup[] = []
-
-  pods.forEach((pod, index) => {
-    const ownerRef = podOwner(pod)
-    if (!ownerRef || !ownerRef?.uid) {
-      discoverPods.push(createDiscoverPod(pod))
-      return
-    }
-
-    const groups = discoverGroups.filter(group => group.uid === ownerRef.uid)
-    if (groups.length > 0) return // pod already processed into a display group
-
-    /*
-     * Pod has an owner uid but not yet processed
-     */
-
-    // find pods with same owner uid as this one
-    const remainingPods = index < pods.length - 1 ? pods.slice(index + 1) : []
-    const replicas = podsWithOwner(remainingPods, ownerRef)
-
-    const theExDiscoverGroups = exDiscoverGroups.filter(group => {
-      return (
-        group.uid === pod.getMetadata()?.uid &&
-        group.namespace === pod.getMetadata()?.namespace &&
-        group.name === ownerRef?.name
-      )
-    })
-
-    // Determine if group previously expanded
-    const expanded = theExDiscoverGroups.length > 0 ? theExDiscoverGroups[0].expanded : true
-
-    discoverGroups.push(toDiscoverGroup(pod, ownerRef, replicas, expanded))
-  })
-
-  return [discoverGroups, discoverPods]
-}
-
-export function filterAndGroupPods(
-  theFilters: TypeFilter[],
-  currentGroups: DiscoverGroup[],
-): [DiscoverGroup[], DiscoverPod[]] {
-  const pods: ManagedPod[] = mgmtService.pods || []
-
-  if (!theFilters || theFilters.length === 0) return groupPodsByDeployment(pods, currentGroups)
-
-  const filtered = pods.filter(pod => {
-    let status = true
-    for (const filter of theFilters) {
-      if (!applyFilter(filter, pod)) {
-        // service fails filter so return
-        status = false
-        break
-      }
-
-      // service passes this filter so continue
-    }
-    return status
-  })
-
-  return groupPodsByDeployment(filtered, currentGroups)
-}
-
-export function getStatus(pod: DiscoverPod): string {
-  return mgmtService.podStatus(pod.mPod)
-}
-
 export enum ViewType {
   listView = 'listView',
   cardView = 'cardView',
 }
+
+class DiscoverService {
+  private discoverGroups: DiscoverGroup[] = []
+  private discoverPods: DiscoverPod[] = []
+
+  private recordValue(values: Record<string, string> | undefined, key: string): string | undefined {
+    if (!values) return undefined
+    return values[key]
+  }
+
+  private toDiscoverGroup(
+    pod: ManagedPod,
+    owner: OwnerReference,
+    replicas: ManagedPod[],
+    expanded: boolean,
+  ): DiscoverGroup {
+    const discoverReplicas = [pod, ...replicas].map(replica => this.createDiscoverPod(replica))
+    return {
+      type: DiscoverType.Group,
+      name: owner.name,
+      uid: owner.uid,
+      namespace: pod.getMetadata()?.namespace || '<unknown>',
+      replicas: discoverReplicas || [],
+      expanded: expanded,
+      config: this.recordValue(pod.getMetadata()?.annotations, 'openshift.io/deployment-config.name'),
+      version: this.recordValue(pod.getMetadata()?.annotations, 'openshift.io/deployment-config.latest-version'),
+      statefulset: this.recordValue(pod.getMetadata()?.labels, 'statefulset.kubernetes.io/pod-name'),
+    }
+  }
+
+  private createDiscoverPod(pod: ManagedPod): DiscoverPod {
+    return {
+      type: DiscoverType.Pod,
+      name: pod.getMetadata()?.name || '<unknown>',
+      uid: pod.getMetadata()?.uid || '<unknown>',
+      namespace: pod.getMetadata()?.namespace || '<unknown>',
+      labels: pod.getMetadata()?.labels || {},
+      annotations: pod.getMetadata()?.annotations || {},
+      mPod: pod,
+    }
+  }
+
+  private podOwner(pod: ManagedPod): OwnerReference | null {
+    const metadata = pod.getMetadata()
+    if (!metadata || !metadata.ownerReferences) return null
+
+    if (metadata.ownerReferences.length === 0) return null
+
+    return metadata.ownerReferences[0]
+  }
+
+  private podsWithOwner(remainingPods: ManagedPod[], ownerRef: OwnerReference): ManagedPod[] {
+    if (!remainingPods) return []
+
+    return remainingPods.filter(pod => {
+      const oRef = this.podOwner(pod)
+      return oRef?.uid === ownerRef.uid
+    })
+  }
+
+  private groupPodsByDeployment(pods: ManagedPod[]) {
+    const discoverPods: DiscoverPod[] = []
+    const discoverGroups: DiscoverGroup[] = []
+
+    for (let i = 0; i < pods.length; ++i) {
+      const pod = pods[i]
+      const ownerRef = this.podOwner(pod)
+
+      if (!ownerRef || !ownerRef?.uid) {
+        discoverPods.push(this.createDiscoverPod(pod))
+        continue
+      }
+
+      const groups = discoverGroups.filter(group => group.uid === ownerRef.uid)
+      if (groups.length > 0) continue // pod already processed into a display group
+
+      /*
+       * Pod has an owner uid but not yet processed
+       */
+
+      // find pods with same owner uid as this one
+      const remainingPods = i < pods.length - 1 ? pods.slice(i + 1) : []
+      const replicas = this.podsWithOwner(remainingPods, ownerRef)
+
+      const oldDiscoverGroups = this.discoverGroups.filter(group => {
+        return (
+          group.uid === pod.getMetadata()?.uid &&
+          group.namespace === pod.getMetadata()?.namespace &&
+          group.name === ownerRef?.name
+        )
+      })
+
+      // Determine if group previously expanded
+      const expanded = oldDiscoverGroups.length > 0 ? oldDiscoverGroups[0].expanded : true
+
+      discoverGroups.push(this.toDiscoverGroup(pod, ownerRef, replicas, expanded))
+    }
+
+    this.discoverGroups = discoverGroups
+    this.discoverPods = discoverPods
+  }
+
+  private applyFilter(filter: TypeFilter, pod: ManagedPod): boolean {
+    if (!pod.getMetadata()) return false
+
+    const metadata = pod.getMetadata()
+    if (!metadata) return false
+
+    type KubeObjKey = keyof typeof metadata
+
+    const podProp = metadata[filter.type.toLowerCase() as KubeObjKey] as string
+
+    // Want to filter on this property but value
+    // is null so filter fails
+    if (!podProp) return false
+
+    return podProp.toLowerCase().includes(filter.value.toLowerCase())
+  }
+
+  filterAndGroupPods(theFilters: TypeFilter[]): [discoverGroups: DiscoverGroup[], discoverPods: DiscoverPod[]] {
+    const pods: ManagedPod[] = mgmtService.pods || []
+
+    let filtered = pods
+    if (theFilters && theFilters.length > 0) {
+      filtered = pods.filter(pod => {
+        let status = true
+        for (const filter of theFilters) {
+          if (!this.applyFilter(filter, pod)) {
+            // service fails filter so return
+            status = false
+            break
+          }
+
+          // service passes this filter so continue
+        }
+        return status
+      })
+    }
+
+    this.groupPodsByDeployment(filtered)
+
+    /**
+     * Notify event service of any errors in the groups and pods
+     */
+    this.discoverGroups.flatMap(discoverGroup =>
+      discoverGroup.replicas.map(discoverPod => discoverPod.mPod.errorNotify()),
+    )
+
+    this.discoverPods.map(discoverPod => discoverPod.mPod.errorNotify())
+
+    return [this.discoverGroups, this.discoverPods]
+  }
+
+  getStatus(pod: DiscoverPod): string {
+    return mgmtService.podStatus(pod.mPod)
+  }
+
+  unwrap(error: Error): string {
+    if (!error) return 'unknown error'
+    if (error.cause instanceof Error) return this.unwrap(error.cause)
+    return error.message
+  }
+}
+
+export const discoverService = new DiscoverService()
