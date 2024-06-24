38f97a9ee24b960a95c2afb9a1a4241574a87c54
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-06-24 16:49:15 +0100

    feat(gateway): Prepare gateway for product building
    * Renames Dockerfile to delineate the now 2 docker files
    
    * Adds gateway image building to Makefile
    
    * gateway/package.json
     * Sorts out dependencies and dev dependencies for webpack bundling
    
    * Prefer webpack over tsup due to latter having problems with dealing with
      jsdom, producing errors
    
    * logger.ts
     * Eliminate use of file for logging as this will require mounting a state
       volume in the deployment and not necessary
    
    * package.json
     * Adds gateway package commands to main scripts

--- docker/gateway/src/logger.ts
+++ docker/gateway/src/logger.ts
@@ -1,12 +1,5 @@
 import pino from 'pino'
-import * as pinoms from 'pino-multi-stream'
 import expressPinoLogger from 'express-pino-logger'
-import * as fs from 'fs'
 
-const streams = [
-  { stream: process.stdout },
-  { stream: fs.createWriteStream('gateway-api.log', { flags: 'a' }) },
-]
-
-export const logger = pino({ level: process.env.LOG_LEVEL || 'info' }, pinoms.multistream(streams))
+export const logger = pino({ level: process.env.LOG_LEVEL || 'info' })
 export const expressLogger = expressPinoLogger(logger)
---
+++ docker/gateway/webpack.config.prod.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+
+  devtool: 'source-map',
+  entry: 'src/gateway-api.ts',
+  target: 'node',
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: './public',
+    pathinfo: true,
+    filename: 'gateway-api.js',
+    chunkFilename: 'static/js/[name].chunk.js',
+    assetModuleFilename: 'static/media/[name].[hash][ext]',
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              noEmit: false, // this option will solve the issue
+            },
+          },
+        },
+        exclude: /node_modules|\.d\.ts$/, // this line as well
+      }
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, './tsconfig.json'),
+      }),
+    ],
+    symlinks: false,
+    cacheWithContext: false
+  },
+
+  // For suppressing warnings that stop app running
+  ignoreWarnings: [
+    // For suppressing sourcemap warnings coming from some dependencies
+    /Failed to parse source map/,
+    /Critical dependency: the request of a dependency is an expression/,
+  ],
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public/ACL.yaml' }],
+    }),
+    new CompressionPlugin({
+      threshold: 8192,
+    }),
+  ]
+}
