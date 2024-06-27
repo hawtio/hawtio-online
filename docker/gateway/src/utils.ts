c215e2a77ff1825aa28f4cd6be96d3eb6ee184ee
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-06-27 21:44:30 +0100

    feat(gateway): Rework proxy server
    * Replaces express-http-proxy with http-proxy-middleware since latter
      supports websockets
    
    * deployment
     * Exposes APP_PORT to env vars so that it can be changed in both the
       app server and nginx's config
     * Adds certificates to convert app server to https
     * Provides to app server config the uri of the nginx server
     * Adds probes for checking status of app server
    
    * env.product
     * Since the certificate is self-signed, need to turn off node verification
       rejection
    
    * package.json
     * Refactor the script names since they can be called from root package
    
    * proxy-dev-server
     * Migrate to http-proxy-middleware
    
    * gateway-api.ts
     * Implements https capability for production install while keeping dev
       as http
     * The /master (nginx) -> /master (gateway) -> /masterinternal (nginx) ->
       cluster-api will not work with websockets so instead have the gateway
       connect to cluster-api directly using the http-proxy-middleware
     * Puts the express.json at the end to ensure it is not overwritten by the
       proxy and since jolokia sends response with 'text/json' broaden it to
       a wildcard
    
    * jolokia-agent.ts
     * Provides tracing in the functions for future debugging
    
    * master-guard.ts
     * Reduces the scope so that it returns a boolean as to whether a given
       path passes or not
    
    * nginx-gateway.conf.template
     * Eliminates the masterinternal as no longer needed since the gateway
       takes care of proxying to the cluster for most queries
     * Adds allow directives for ipv6 localhost as well as ipv4

---
+++ docker/gateway/src/utils.ts
@@ -0,0 +1,33 @@
+export function isObject(value: unknown): value is object {
+  if (! value) return false
+
+  const type = typeof value
+  return value != null && (type === 'object' || type === 'function')
+}
+
+export function cloneObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value))
+}
+
+export function isRecord(value: unknown): value is Record<string, unknown> {
+  return isObject(value) && (typeof value === 'object')
+}
+
+export function isString(value: unknown): value is string {
+  return typeof value === 'string' || value instanceof String
+}
+
+export function toStringArray(value: unknown): string[] {
+  if (! Array.isArray(value)) return []
+
+  const strings: string[] = []
+  value.forEach(v => {
+    strings.push(v.toString())
+  })
+
+  return strings
+}
+
+export function isError(obj: unknown): obj is Error {
+  return obj instanceof Error
+}
