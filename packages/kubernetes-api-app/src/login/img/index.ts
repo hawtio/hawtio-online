86ce7a74c46426658e8e5dcbd9ccf0b70badf284
mmelko <mmelko@redhat.com>
2024-07-03 15:37:37 +0200

    feat: Upgrade Patternfly to v5 with necessary refactors upgrade hawtio/react to 1.3.0

--- packages/kubernetes-api-app/src/login/img/index.ts
+++ packages/kubernetes-api-app/src/login/img/index.ts
@@ -1,18 +1,5 @@
-import { BackgroundImageSrcMap } from '@patternfly/react-core'
 import hawtioLogo from './hawtio-logo.svg'
 import userAvatar from './img_avatar.svg'
-import backgroundImageSrcLg from './pfbg_1200.jpg'
-import backgroundImageSrcXs from './pfbg_576.jpg'
-import backgroundImageSrcXs2x from './pfbg_576@2x.jpg'
-import backgroundImageSrcSm from './pfbg_768.jpg'
-import backgroundImageSrcSm2x from './pfbg_768@2x.jpg'
+import background from './background.svg'
 
-export const backgroundImages: BackgroundImageSrcMap = {
-  xs: backgroundImageSrcXs,
-  xs2x: backgroundImageSrcXs2x,
-  sm: backgroundImageSrcSm,
-  sm2x: backgroundImageSrcSm2x,
-  lg: backgroundImageSrcLg,
-}
-
-export { hawtioLogo, userAvatar }
+export { hawtioLogo, userAvatar, background }
