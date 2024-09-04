6bd873cb74195ce08835a82ed51fa5b430e1148c
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-09-04 13:49:25 +0100

    Updates the management API and management test app

--- packages/management-api/src/globals.ts
+++ packages/management-api/src/globals.ts
@@ -1,4 +1,6 @@
 import { Logger } from '@hawtio/react'
+import { ManagedPod } from './managed-pod'
+import { ManagedProject } from './managed-project'
 
 export const pluginName = 'hawtio-online-management-api'
 export const log = Logger.get(pluginName)
@@ -8,4 +10,29 @@ export const log = Logger.get(pluginName)
  */
 export enum MgmtActions {
   UPDATED = 'UPDATED',
+}
+
+export type MPodsByUid = { [uid: string]: ManagedPod }
+
+export type ManagedProjects = { [key: string]: ManagedProject }
+
+export enum TypeFilterType {
+  NAME = 'Name',
+  NAMESPACE = 'Namespace',
+}
+
+export function typeFilterTypeValueOf(str: string): TypeFilterType | undefined {
+  switch (str) {
+    case TypeFilterType.NAME:
+      return TypeFilterType.NAME
+    case TypeFilterType.NAMESPACE:
+      return TypeFilterType.NAMESPACE
+    default:
+      return undefined
+  }
+}
+
+export type TypeFilter = {
+  type: TypeFilterType
+  values: Set<string>
 }
