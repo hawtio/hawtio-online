d5e2dab5b85f81dee3cde1f8feba1de3b0dc0354
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2024-09-10 19:10:53 +0100

    Exposes Discover Preferences (#496)
    * Allows users to change the number of pods displayed in each Discover
      tab and the polling interval to determine heartbeat to jolokia container
      ports

--- packages/online-shell/src/discover/index.ts
+++ packages/online-shell/src/discover/index.ts
@@ -2,14 +2,21 @@ import { hawtio, HawtioPlugin } from '@hawtio/react'
 import { pluginPath } from './globals'
 import { Discover } from './Discover'
 import { HeaderMenuDropDown } from './HeaderMenuDropDown'
+import { preferencesRegistry } from '@hawtio/react'
+import { DiscoverPreferences } from './DiscoverPreferences'
+
+const pluginId = 'discover'
+const pluginTitle = 'Discover'
 
 export const discover: HawtioPlugin = () => {
   hawtio.addPlugin({
-    id: 'discover',
-    title: 'Discover',
+    id: pluginId,
+    title: pluginTitle,
     path: pluginPath,
     component: Discover,
     headerItems: [{ component: HeaderMenuDropDown, universal: true }],
     isActive: async () => true,
   })
+
+  preferencesRegistry.add(pluginId, pluginTitle, DiscoverPreferences)
 }
