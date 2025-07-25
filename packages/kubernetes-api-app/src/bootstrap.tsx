625da3a63784e3b373cd75a86069c255e93104c7
Paul Richardson <p.g.richardson@phantomjinx.co.uk>
2025-07-25 13:40:39 +0100

    fix: Drops use of web-vitals

--- packages/kubernetes-api-app/src/bootstrap.tsx
+++ packages/kubernetes-api-app/src/bootstrap.tsx
@@ -2,7 +2,6 @@ import { Logger, configManager, hawtio } from '@hawtio/react'
 import { isK8ApiRegistered, log } from '@hawtio/online-kubernetes-api'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { reportWebVitals } from './reportWebVitals'
 import { Kubernetes } from './Kubernetes'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthLoginPage } from './login'
@@ -36,9 +35,4 @@ isK8ApiRegistered().then(() => {
       </BrowserRouter>
     </React.StrictMode>,
   )
-
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals()
 })
